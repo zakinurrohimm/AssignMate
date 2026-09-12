e8ab15caf11e536578e4c558b8764bee68c0fd52
Zucc <zakinurrohimm@gmail.com>
2026-09-12 13:50:57 +0700

    ngubah nama doang

--- manajemen-tugas/resources/views/auth/login.blade.php
+++ manajemen-tugas/resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SYSTEM_AUTHENTICATION // ASSIGNMATE</title>
+    <title>ASSIGNMATE - LOGIN</title>
     <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
     @vite('resources/css/app.css')
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
--- manajemen-tugas/resources/views/auth/register.blade.php
+++ manajemen-tugas/resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INITIALIZE_OPERATOR // ASSIGNMATE</title>
+    <title>ASSIGNMATE - REGISTER</title>
     <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
     @vite('resources/css/app.css')
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
